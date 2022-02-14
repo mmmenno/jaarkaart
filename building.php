f317efa6af737c570255a94bdf3e307d9a411228
Menno den Engelse <menno@denengelse.nl>
2022-02-14 11:46:55 +0100

    use curl

--- building.php
+++ building.php
@@ -57,6 +57,7 @@ $querylink = "https://druid.datalegend.net/AdamNet/all/sparql/endpoint#query=" .
 
 
 // Druid does not like url parameters, send accept header instead
+/*
 $opts = [
     "http" => [
         "method" => "GET",
@@ -68,6 +69,22 @@ $context = stream_context_create($opts);
 
 // Open the file using the HTTP headers set above
 $json = file_get_contents($url, false, $context);
+*/
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch,CURLOPT_USERAGENT,'adamlink');
+$headers = [
+  'Accept: application/sparql-results+json'
+];
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$json = curl_exec ($ch);
+curl_close ($ch);
 
 $data = json_decode($json,true);
 
